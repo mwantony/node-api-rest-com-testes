937bb57a8634fde64d7bff05c6a1720b0ea22084
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-27 18:42:47 -0300

    cap 5 vid 2 finalizado

---
+++ src/test/models/editora.test.js
@@ -0,0 +1,37 @@
+import { describe, expect, it } from '@jest/globals';
+import Editora from '../../models/editora.js';
+
+describe('Testando o modelo Editora', () => {
+  const objetoEditora = {
+    nome: 'CDC',
+    cidade: 'São Paulo',
+    email: 'c@c.com',
+  };
+
+  it('Deve instanciar uma nova editora', () => {
+    const editora = new Editora(objetoEditora);
+
+    expect(editora).toEqual(expect.objectContaining(objetoEditora));
+  });
+
+  it.skip('Deve salvar editora no banco de dados', () => {
+    const editora = new Editora(objetoEditora);
+    editora.salvar().then((dados) => {
+      expect(dados.nome).toBe('CDC');
+    });
+  });
+
+  it('Deve salvar na database usando a sintaxe moderna', async () => {
+    const editora = new Editora(objetoEditora);
+    const dados = await editora.salvar();
+    const retornado = await Editora.pegarPeloId(dados.id);
+    expect(retornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
+    );
+  });
+});
